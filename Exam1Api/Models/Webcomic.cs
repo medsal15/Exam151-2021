7c3aec7f3f89d259629f59a8361a852be9826a94
medsal15 <medsal15@users.noreply.github.com>
2021-01-07 12:24:47 +0100

    Initial Commit

---
+++ Exam1Api/Models/Webcomic.cs
@@ -0,0 +1,9 @@
+namespace Exam1Api.Models
+{
+    public class Webcomic
+    {
+        public int Id { get; set; };
+        public int AuthorId { get; set; };
+        public string Name { get; set; };
+    }
+}
